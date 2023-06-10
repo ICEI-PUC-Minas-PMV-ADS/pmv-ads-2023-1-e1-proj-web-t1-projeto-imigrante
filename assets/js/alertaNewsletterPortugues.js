a851280c01469c587286cd1938e49bee20dd8eeb
ViniciusDG <128438209+ViniciusDG@users.noreply.github.com>
2023-06-10 03:05:13 -0300

    newsletter portugues
    acrescimo da funcionalidade newsletter nas páginas em português

---
+++ assets/js/alertaNewsletterPortugues.js
@@ -0,0 +1,47 @@
+//Acessa o campo do email
+let emailNewsletter = document.getElementById("email-news");
+
+// Acessa campo de erro no html
+let emailNewsletterError = document.getElementById("emailNewsError");
+
+// Caracteres para validar se o email é válido
+const emailIsValidNews = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+
+
+
+emailNewsletter.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    showAlertMessage();
+
+});
+
+
+// Funcao p validar campo email
+function validateNewsletterInput() {
+    const valueNews = emailNewsletter.value.trim();
+    let errorMessageNews = '';
+    let isValidNews = false;
+
+    // Verifica se o email é válido
+    if (!valueNews.match(emailIsValidNews)) {
+        errorMessageNews = 'Email inválido';
+        isValidNews = false;
+    } else {
+        isValidNews = true;
+    }
+    emailNewsletterError.innerHTML = errorMessageNews;
+
+    return isValidNews;
+}
+
+
+
+//Funcao p mostrar a tela de alerta se o email for preenchido
+function showAlertMessage() {
+    if (validateNewsletterInput()) {
+        alert("Email cadastrado com sucesso!");
+        emailNewsletter.value = "";
+    }
+}
+
