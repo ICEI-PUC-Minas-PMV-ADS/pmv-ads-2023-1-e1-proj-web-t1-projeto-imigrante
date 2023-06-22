6fd233d1e824522cc4de140d49335fc9e974c008
alissa <alissanishihara@gmail.com>
2023-06-22 17:32:43 +0200

    implement list objects in english page

--- src/assets/js/object.js
+++ src/assets/js/object.js
@@ -2,111 +2,110 @@
 const contacts = [
     {
         jesuitas: {
-            website: "Site: www.sjmrbrasil.org",
-            adress: 'Endereço: Avenida Amazonas, 641 - 8º Andar, Centro - Belo Horizonte/MG',
-            phone: 'Telefone(s): +55 (31) 3212-45',
-            docsProtect: 'Proteção Documental: (31) 99210-3444 e 99210-3443',
-            juridic: "Proteção Jurídica: juridicobh@sjmrbrasil.org e (31) 99210-3435",
-            social: "Proteção Social: socialbh@sjmrbrasil.org e (31) 99210-3434",
-            survive: "Meios de Vida: trabalhobh@sjmrbrasil.org e (31) 99428-0043",
-            integration: "Integração (Línguas e Cultura): (Línguas e Cultura): cultura@sjmrbrasil.org e (31) 99927-5507"
+            website: "Website: www.sjmrbrasil.org",
+            address: 'Address: Avenida Amazonas, 641 - 8th Floor, Centro - Belo Horizonte/MG',
+            phone: 'Phone(s): +55 (31) 3212-45',
+            docsProtect: 'Document Protection: (31) 99210-3444 and 99210-3443',
+            juridic: "Legal Protection: juridicobh@sjmrbrasil.org and (31) 99210-3435",
+            social: "Social Protection: socialbh@sjmrbrasil.org and (31) 99210-3434",
+            survive: "Livelihood: trabalhobh@sjmrbrasil.org and (31) 99428-0043",
+            integration: "Integration (Languages and Culture): (Languages and Culture): cultura@sjmrbrasil.org and (31) 99927-5507"
         }
     },
     {
         fraternidade: {
-            website: "Site: www.missoeshumanitarias.org",
-            adress: "Endereço: Rua Presidente Antônio Carlos, 400 - Carmo da Cachoeira, MG",
-            phone: "Telefone(s): +55 (35) 9719-7076 | +55 (35) 3225-1233",
-            email: "E-mail: executivo@fraterinternacional.org"
+            website: "Website: www.missoeshumanitarias.org",
+            address: "Address: Rua Presidente Antônio Carlos, 400 - Carmo da Cachoeira, MG",
+            phone: "Phone(s): +55 (35) 9719-7076 | +55 (35) 3225-1233",
+            email: "Email: executivo@fraterinternacional.org"
         }
-
     },
     {
         jesuitasRoraima: {
-            website: "Site: www.sjmrbrasil.org",
-            adress: "Endereço: Avenida General Ataíde Teive, 2386 A - Liberdade ",
-            phone: "Telefone: (95) 99962-1000 ",
-            email: "coordenadorbv@sjmrbrasil.org"
+            website: "Website: www.sjmrbrasil.org",
+            address: "Address: Avenida General Ataíde Teive, 2386 A - Liberdade",
+            phone: "Phone: (95) 99962-1000",
+            email: "Email: coordenadorbv@sjmrbrasil.org"
         }
     },
     {
         jesuitasManaus: {
-            website: "Site: www.sjmrbrasil.org",
-            adress: "Endereço: Av. Constatino Nery, nº 1029 - São Geraldo ",
-            phone: "Telefone: (92) 99157-6097 ",
-            email: "atendimentomao@sjmrbrasil.org"
+            website: "Website: www.sjmrbrasil.org",
+            address: "Address: Av. Constatino Nery, No. 1029 - São Geraldo",
+            phone: "Phone: (92) 99157-6097",
+            email: "Email: atendimentomao@sjmrbrasil.org"
         }
     },
-
     {
-       migracoesDf: {
-            website: "Site: www.sjmrbrasil.org",
-            adress: "Quadra 7 Conjunto C Lote 1, Vila Varjão - Lago Norte - Brasília/DF",
-            phone: " Telefone(s): +55 (61) 3340-2689 | +55 (61) 3447-8043 | +55 (61) 98173-7688",
-            email: "E-mail: imdh@migrante.org.br  | imdh.diretoria@migrante.org.br"
+        migracoesDf: {
+            website: "Website: www.sjmrbrasil.org",
+            address: "Address: Quadra 7 Conjunto C Lote 1, Vila Varjão - Lago Norte - Brasília/DF",
+            phone: "Phone(s): +55 (61) 3340-2689 | +55 (61) 3447-8043 | +55 (61) 98173-7688",
+            email: "Email: imdh@migrante.org.br | imdh.diretoria@migrante.org.br"
         }
     },
     {
         migracoesRs: {
-             website: "Site: www.sjmrbrasil.org",
-             adress: "Endereço: Rua General Caldwell, nº 651, Bairro Menino Deus",
-             phone: "Telefone: (51) 3254-0140 e (51) 9995-5573 (WhatsApp) ",
-             email: "E-mail: atendimentopoa@sjmrbrasil.org"
-         }
-     },
-     {
+            website: "Website: www.sjmrbrasil.org",
+            address: "Address: Rua General Caldwell, No. 651, Bairro Menino Deus",
+            phone: "Phone: (51) 3254-0140 and (51) 9995-5573 (WhatsApp)",
+            email: "Email: atendimentopoa@sjmrbrasil.org"
+        }
+    },
+    {
         caritasRj: {
-             website: "Site: www.caritas-rj.org.br",
-             adress: "Endereço: Rua General Caldwell, nº 651, Bairro Menino Deus",
-             phone: "Telefone: (51) 3254-0140 e (51) 9995-5573 (WhatsApp) ",
-             email: "E-mail: www.caritas-rj.org.br/contatos.html"
-         }
-     },
-     {
+            website: "Website: www.caritas-rj.org.br",
+            address: "Address: Rua General Caldwell, No 651, Bairro Menino Deus",
+            phone: "Phone: (51) 3254-0140 and (51) 9995-5573 (WhatsApp)",
+            email: "Email: www.caritas-rj.org.br/contatos.html"
+        }
+    },
+    {
         caritasSp: {
-             website: "Site: www.sjmrbrasil.org",
-             adress: " Endereço: Avenida Marechal Eurico Gaspar Dutra, 1853, Parada Inglesa",
-             phone: " Telefone(s): +55 (11) 93904-5321",
-             protecao: " Proteção Documental: (31) 99210-3444 e 99210-3443",
-             email: "E-mail: caritassp@caritassp.org.br"
-         }
-     },
-     {
+            website: "Website: www.sjmrbrasil.org",
+            address: "Address: Avenida Marechal Eurico Gaspar Dutra, 1853, Parada Inglesa",
+            phone: "Phone(s): +55 (11) 93904-5321",
+            protection: "Document Protection: (31) 99210-3444 and 99210-3443",
+            email: "Email: caritassp@caritassp.org.br"
+        }
+    },
+    {
         cic: {
-             website: "Site: https://justica.sp.gov.br/index.php/coordenacoes-e-programas/integracao-da-cidadania-cic/",
-             adress: "Endereço: Rua Barra Funda, 1020 - Barra Funda",
-             phone: "Telefone: +55 (11) 3115-2048 55",
-             email: " E-mail: cicdoimigrante@sp.gov.br ",
-             atividades: "Atividades: atendimento a imigrantes e a refugiados, aulas de português, capacitação profissional, orientação sobre documentação e sobre Sisconare",
-             horário: "Horário de atendimento: das 8h às 17h"
-         }
-     },
-     {
+            website: "Website: https://justica.sp.gov.br/index.php/coordenacoes-e-programas/integracao-da-cidadania-cic/",
+            address: "Address: Rua Barra Funda, 1020 - Barra Funda",
+            phone: "Phone: +55 (11) 3115-2048 55",
+            email: "Email: cicdoimigrante@sp.gov.br",
+            activities: "Activities: assistance to immigrants and refugees, Portuguese classes, professional training, guidance on documentation and Sisconare",
+            hours: "Service hours: 8 AM to 5 PM"
+        }
+    },
+    {
         compassiva: {
-             website: "Site:compassiva.org.br",
-             adress: "Endereço: Rua da Glória, 900 - Liberdade",
-             phone: "Telefone(s): +55 (11) 2537-3441",
-             email: "E-mail: contato@compassiva.org.br",
-             atividades: "Atividades desenvolvidas: revalidação de diplomas de graduação de refugiados."
-         }
-     },
-     {
-       crai: {
-             website: "Site: www.prefeitura.sp.gov.br/cidade/secretarias/direitos_humanos/imigrantes_e_trabalho_decente/crai",
-             adress: "Endereço: Rua Major Diogo, 834, Bela Vista",
-             phone: "Telefones: +55 (11) 2361-3780 | +55 (11) 2361-5069",
-             email: "E-mail: crai@sefras.org.br"  
-         }
-     },
-     {
+            website: "Website: compassiva.org.br",
+            address: "Address: Rua da Glória, 900 - Liberdade",
+            phone: "Phone(s): +55 (11) 2537-3441",
+            email: "Email: contato@compassiva.org.br",
+            activities: "Activities carried out: revalidation of undergraduate degrees for refugees."
+        }
+    },
+    {
+        crai: {
+            website: "Website: www.prefeitura.sp.gov.br/cidade/secretarias/direitos_humanos/imigrantes_e_trabalho_decente/crai",
+            address: "Address: Rua Major Diogo, 834, Bela Vista",
+            phone: "Phones: +55 (11) 2361-3780 | +55 (11) 2361-5069",
+            email: "Email: crai@sefras.org.br"
+        }
+    },
+    {
         unifesp: {
-             website: "Site: sp.unifesp.br/epm/medicina-preventiva/informes/acolhimento-psicossocial-intercultural",
-             phone: "Whatsapp: +55 (11) 99179-1699",
-             email: "E-mail: intercultural@unifesp.br"
-         }
-     }
-
+            website: "Website: sp.unifesp.br/epm/medicina-preventiva/informes/acolhimento-psicossocial-intercultural",
+            phone: "WhatsApp: +55 (11) 99179-1699",
+            email: "Email: intercultural@unifesp.br"
+        }
+    }
 ];
+
+
 
 
 const unifespElement = document.getElementById("unifesp");
